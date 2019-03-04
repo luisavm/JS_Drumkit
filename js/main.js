59c01230631dc0a24220813b35bad711f6f5257a
Luisa Valero <luisa.valero19@gmail.com>
2019-03-04 16:15:52 -0500

    Updating UI

--- js/main.js
+++ js/main.js
@@ -10,11 +10,34 @@
 		// debugging/error handling
         // if we don't have a matching audio element, then kill the function
         // ! is a "not operator or a bang
-        if(!audio) {return; } // return stops doce execution 
+        if(!audio) {return; }
+         // return stops doce execution 
 		// rewind audio on every click and make it play 
         audio.currentTime = 0;
 		audio.play();
+
+		//grab the div and animate it
+		let key = document.querySelector(`div[data-key="${event.keyCode}"]`);
+		key.classList.add('playing')
+
 	}
 
-	window.addEventListener("keydown", playDrumkitSound)
+	function removePlayingClass(event) {
+		// debugger;
+		if (event.propertyName !== "transform") {
+		return;
+		} else {
+			// remove the playing class here from the active div
+			console.log('transition is done!', `${event.propertyName}`);
+			event.target.classList.remove('playing');
+		}
+	}
+
+	const keys = Array.from(document.querySelectorAll('.keys'));
+
+	// keys.forEach(key => key.addEventListener('keys'));
+    keys.forEach(key => key.addEventListener("transitionend", removePlayingClass));
+
+	window.addEventListener("keydown", playDrumkitSound);
+
 })();
